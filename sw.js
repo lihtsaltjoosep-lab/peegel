53c1e391be4a11448b8922d0541f49d33c708a64
lihtsaltjoosep-lab <lihtsaltjoosep@gmail.com>
2026-01-23 13:42:50 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+// sw.js - See peab olema eraldi failis
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    // See hoiab teenuse ärvel
+});
