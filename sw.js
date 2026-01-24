282e79ddf7b6bbfb249a1d7ed6c8654ea61e8646
lihtsaltjoosep-lab <lihtsaltjoosep@gmail.com>
2026-01-24 16:31:40 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,21 @@
 // sw.js - Peegel Pro Stability Engine
-const CACHE_NAME = 'peegel-v121';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'peegel-v33-bg';
 
-// Installimine ja failide puhverdamine
 self.addEventListener('install', (event) => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+    self.skipWaiting();
 });
 
-// Aktiveerimine ja vana vahemälu puhastus
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
-  );
+    event.waitUntil(clients.claim());
 });
 
-// Sidetee hoidmine põhiäpiga (Ping-Pong loogika)
+// See hoiab workerit elus
 self.addEventListener('message', (event) => {
-  if (event.data.type === 'PING') {
-    event.source.postMessage({ type: 'PONG' });
-  }
+    if (event.data.type === 'PING') {
+        event.source.postMessage({ type: 'PONG' });
+    }
 });
 
-// Fetch handler on vajalik PWA installimiseks
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
