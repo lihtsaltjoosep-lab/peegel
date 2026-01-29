e41695457a855fbdcd57e90a5069d570d0a4d77b
lihtsaltjoosep-lab <lihtsaltjoosep@gmail.com>
2026-01-30 00:02:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,1 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  // See on vajalik, et Chrome peaks äppi offline-võimeliseks
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return new Response("Offline mode active");
-    })
-  );
-});
+self.addEventListener('fetch', () => {});
