74640d52c8567430be8fccc8504882dcbf1958f3
lihtsaltjoosep-lab <lihtsaltjoosep@gmail.com>
2026-01-29 20:23:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,3 @@
-// sw.js - Peegel Pro Stability Engine
-const CACHE_NAME = 'peegel-v33-bg';
-
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim());
-});
-
-// See hoiab workerit elus
-self.addEventListener('message', (event) => {
-    if (event.data.type === 'PING') {
-        event.source.postMessage({ type: 'PONG' });
-    }
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+self.addEventListener('fetch', function(event) {
+  // Tühi Service Worker, mis on vajalik PWA installimiseks
 });
