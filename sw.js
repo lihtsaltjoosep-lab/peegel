fd640b325e47c71efac50e4e93cc10079c6d7a39
lihtsaltjoosep-lab <lihtsaltjoosep@gmail.com>
2026-01-31 12:43:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
-const CACHE_NAME = 'peegel-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdn.tailwindcss.com'
-];
+const CACHE_NAME = 'peegel-v2';
+const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
