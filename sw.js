9c063492c42eda7e853dd47f3fbdcfa84346e3b2
lihtsaltjoosep-lab <lihtsaltjoosep@gmail.com>
2026-01-31 13:10:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,1 @@
-const CACHE_NAME = 'peegel-v2';
-const ASSETS = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+self.addEventListener('fetch', () => {});
